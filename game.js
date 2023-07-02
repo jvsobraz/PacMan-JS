fa88b339f7bde2e71e367ac9d3140c662efe2c58
jvsobraz <jvsobraz@hotmail.com>
2023-07-02 13:30:11 -0300

    Update game.js

--- game.js
+++ game.js
@@ -61,7 +61,7 @@ let map = [
 let randomTargetsForGhosts = [
     { x: 1 * oneBlockSize, y : 1 * oneBlockSize },
     { x: 1 * oneBlockSize, y: (map.length - 2) * oneBlockSize },
-    { x: (map[0]. length - 2) * oneBlockSize, y: oneBlockSize },
+    { x: (map[0].length - 2) * oneBlockSize, y: oneBlockSize },
     {
         x: (map[0].length - 2) * oneBlockSize,
         y: (map.length - 2) * oneBlockSize,
